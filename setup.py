fe486ee8378fe8bbc0edf15b252c9bb2a6bc77e9
Ari Hershowitz <arihershowitz@gmail.com>
2021-11-04 16:26:46 -0700

    Bump to v0.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='billtitles',
-    version='0.0.4',
+    version='0.0.5',
     packages=['billtitles'],
     install_requires=[
         'importlib; python_version == "3.8"',
