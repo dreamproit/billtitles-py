67cb674b27572cc0def908b841567d848e60349f
Ari Hershowitz <arihershowitz@gmail.com>
2021-11-04 16:29:47 -0700

    Bump to v0.1.1 to match docker version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='billtitles',
-    version='0.0.5',
+    version='0.1.1',
     packages=['billtitles'],
     install_requires=[
         'importlib; python_version == "3.8"',
