4761e5d374a675c875f95a16ec3de4aae9b71884
aih <arihershowitz@gmail.com>
2021-12-19 15:57:08 -0800

    Modify to work with pgsql docker image

--- billtitles/database.py
+++ billtitles/database.py
@@ -5,7 +5,8 @@ from sqlmodel import create_engine
 
 from . import constants
 
-postgres_url = f"postgresql://postgres:{constants.POSTGRES_PW}@localhost/billsim"
+#postgres_url = f"postgresql://postgres:{constants.POSTGRES_PW}@localhost/billsim"
+postgres_url = f"postgresql://postgres:{constants.POSTGRES_PW}@localhost"
 
 engine = create_engine(postgres_url, echo=True)
 
