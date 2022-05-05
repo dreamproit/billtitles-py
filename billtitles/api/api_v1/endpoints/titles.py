23e166362dedcc1c324569ca2dc7bbc1544fb344
Alexander Boiko <django.develop@gmail.com>
2022-05-05 11:32:01 +0300

    Fix django.setup()

--- billtitles/api/api_v1/endpoints/titles.py
+++ billtitles/api/api_v1/endpoints/titles.py
@@ -1,1 +1,14 @@
-from billtitleindex.btiapp.endpoints.titles import router
+import os
+import django
+
+if not os.environ.get("DJANGO_SETTINGS_MODULE"):
+    os.environ["DJANGO_SETTINGS_MODULE"] = "billtitles.billtitles.settings"
+
+django.setup()
+
+from billtitleindex.btiapp.endpoints.titles import router as titles_router
+
+for route in titles_router.routes:
+    if route.tags:
+        route.tags = []
+router = titles_router
